5feb367ea1fb8bc38803fbebf89b94662fb945a3
thalita-auad <thalita.auad@tribofox.com.br>
2025-03-10 15:05:09 -0300

    Initial Commit

---
+++ TriboFoxTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TriboFoxTest
+//
+//  Created by Thalita Auad on 10/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TriboFoxTest/TriboFoxTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriboFoxTestApp.swift
+//  TriboFoxTest
+//
+//  Created by Thalita Auad on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TriboFoxTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
